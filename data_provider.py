3d3cb5b614ada42f5ea98b6ad4de3b6d9b54da87
tk <2774315388@qq.com>
2022-11-10 11:13:57 +0800

    first commit

---
+++ data_provider.py
@@ -0,0 +1,10 @@
+import torch
+import torchvision
+import torchvision.transforms as transforms
+
+transform = transforms.Compose(
+    [transforms.ToTensor(),
+     transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5))])
+
+trainset = torchvision.datasets.CIFAR10(root='./data', train=True,
+                                        download=True, transform=transform)
