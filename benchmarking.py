9b4e62dd0703b42f4116e9344fe34ad05da40e13
tk <2774315388@qq.com>
2022-11-20 09:43:11 +0800

    fix error

--- benchmarking.py
+++ benchmarking.py
@@ -1,9 +1,9 @@
 import benchmarking_utils
-from data_provider import testloader
+from data_provider import val_loader
 import torch
 if __name__ == '__main__':
     model_path = "original_model/resnet56_model.pth"
     net = torch.load(model_path)
     print(f"net is from {model_path}")
     device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
-    benchmarking_utils.inference_benchmark(net, testloader)
+    benchmarking_utils.inference_benchmark(net, val_loader, device)
