d3410746fe2b386ff6df40da09c2babb19e68fad
tk <2774315388@qq.com>
2022-11-21 13:39:40 +0800

    fix save model path

--- benchmarking.py
+++ benchmarking.py
@@ -2,7 +2,7 @@ import benchmarking_utils
 from data_provider import val_loader
 import torch
 if __name__ == '__main__':
-    model_path = "original_model/resnet56_model.pth"
+    model_path = "saved_model/original_model/resnet56_model.pth"
     net = torch.load(model_path)
     print(f"net is from {model_path}")
     device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
--- data_provider.py
+++ data_provider.py
@@ -13,6 +13,9 @@ test_transform = T.Compose([
 
 valset = CIFAR100(root="./data", train=False,
                   transform=test_transform, download=True)
+trainset = CIFAR100(root="./data", train=True,
+                    transform=train_transform, download=True)
+
 
 batch_size = 256
 num_workers = 16
@@ -22,3 +25,8 @@ val_loader = torch.utils.data.DataLoader(valset, batch_size=batch_size,
                                          sampler=val_sampler,
                                          num_workers=num_workers,
                                          persistent_workers=True)
+train_loader = torch.utils.data.DataLoader(trainset, batch_size=batch_size,
+                                           shuffle=(val_sampler is None),
+                                           sampler=val_sampler,
+                                           num_workers=num_workers,
+                                           persistent_workers=True)
