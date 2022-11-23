c56fabcdfd46bb56fd9bd2ef06a8691122a3c745
tk <2774315388@qq.com>
2022-11-23 15:21:47 +0800

    fix data provider transform error

--- data_provider.py
+++ data_provider.py
@@ -11,22 +11,63 @@ test_transform = T.Compose([
 ])
 
 
+def get_train_transforms(mean, std):
+    return T.Compose([
+        T.RandomCrop(32, padding=4),
+        T.RandomHorizontalFlip(),
+        T.ToTensor(),
+        T.Normalize(mean=mean, std=std)
+    ])
+
+
+def get_val_transforms(mean, std):
+    return T.Compose([
+        T.ToTensor(),
+        T.Normalize(mean=mean, std=std)
+    ])
+
+
+def get_vit_train_transforms(mean, std, img_size):
+    return T.Compose([
+        T.RandomResizedCrop((img_size, img_size), scale=(0.05, 1.0)),
+        T.ToTensor(),
+        T.Normalize(mean=[0.5, 0.5, 0.5], std=[0.5, 0.5, 0.5]),
+    ])
+
+
+def get_vit_val_transforms(mean, std, img_size):
+    return T.Compose([
+        T.Resize((img_size, img_size)),
+        T.ToTensor(),
+        T.Normalize(mean=[0.5, 0.5, 0.5], std=[0.5, 0.5, 0.5]),
+    ])
+
+
+mean = [0.5070, 0.4865, 0.4409]
+std = [0.2673, 0.2564, 0.2761]
+
 valset = CIFAR100(root="./data", train=False,
-                  transform=test_transform, download=True)
+                  transform=get_train_transforms(mean, std), download=True)
 trainset = CIFAR100(root="./data", train=True,
-                    transform=train_transform, download=True)
+                    transform=get_val_transforms(mean, std), download=True)
 
 
-batch_size = 1
-num_workers = 16
 val_sampler = None
-val_loader = torch.utils.data.DataLoader(valset, batch_size=batch_size,
-                                         shuffle=(val_sampler is None),
-                                         sampler=val_sampler,
-                                         num_workers=num_workers,
-                                         persistent_workers=True)
-train_loader = torch.utils.data.DataLoader(trainset, batch_size=batch_size,
-                                           shuffle=(val_sampler is None),
-                                           sampler=val_sampler,
-                                           num_workers=num_workers,
-                                           persistent_workers=True)
+
+
+def get_test_loader(batch_size, num_workers):
+    test_loader = torch.utils.data.DataLoader(valset, batch_size=batch_size,
+                                              shuffle=(val_sampler is None),
+                                              sampler=val_sampler,
+                                              num_workers=num_workers,
+                                              persistent_workers=True)
+    return test_loader
+
+
+def get_train_loader(batch_size, num_workers):
+    train_loader = torch.utils.data.DataLoader(trainset, batch_size=batch_size,
+                                               shuffle=(val_sampler is None),
+                                               sampler=val_sampler,
+                                               num_workers=num_workers,
+                                               persistent_workers=True)
+    return train_loader
