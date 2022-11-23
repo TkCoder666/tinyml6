3960bff2e06acbc0df1546229a7dbda5c3e15c6b
LogCreative <logcreative@outlook.com>
2022-11-23 10:27:42 +0800

    only tvmc tuning with AutoTVM

--- tvmc.py
+++ tvmc.py
@@ -9,17 +9,20 @@ from tvm.driver import tvmc
 model = tvmc.load('saved_model/onnx_model/resnet56.onnx')
 
 # Step 2: Compile (Before)
-old_package = tvmc.compile(model, target="llvm", package_path="saved_model/tvm_model/resnet56_before.tar")
+old_package = tvmc.compile(model, target="llvm", package_path="saved_model/tvm_model/resnet56_old.tar")
 
 # Step 3: Run (Before)
 result = tvmc.run(old_package, device="cpu")
 print(result)
 
-# Step 4: Tuning
-tvmc.tune(model, target="llvm", enable_autoscheduler=True)
+# # Step 4: Tuning (AutoScheduler, slow)
+# tvmc.tune(model, target="llvm", enable_autoscheduler=True)
+
+# Step 4: Tuning (AutoTVM, fast)
+tvmc.tune(model, target="llvm")
 
 # Step 2: Compile (After)
-new_package = tvmc.compile(model, target="llvm", package_path="saved_model/tvm_model/resnet56_after.tar")
+new_package = tvmc.compile(model, target="llvm", package_path="saved_model/tvm_model/resnet56_new.tar")
 
 # Step 3: Run (After)
 result = tvmc.run(new_package, device="cpu")
